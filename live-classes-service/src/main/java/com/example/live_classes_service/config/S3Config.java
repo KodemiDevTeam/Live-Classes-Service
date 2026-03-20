e2f9c5753b01f9f0ef3d06de6853e2770c9bc939
goivnd78489 <23e51a67e8@hitam.org>
2026-03-20 13:12:36 +0530

    bugs solved

---
+++ live-classes-service/src/main/java/com/example/live_classes_service/config/S3Config.java
@@ -0,0 +1,16 @@
+package com.example.live_classes_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.EU_NORTH_1)
+                .build();
+    }
+}
